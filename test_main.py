6636197b21cdb3a27fed614fdef93782262cf8d0
Dwayne Bailey <dwayne@translate.org.za>
2015-06-01 00:43:59 +0100

    Add more tests
    Broken ones are commented out as needed.

--- test_main.py
+++ test_main.py
@@ -42,7 +42,46 @@ def test_space_in_key():
 		("key\ with\ spaces b", [("key with spaces", "b")]),
 		("key\ with\ spaces : b", [("key with spaces", "b")]),
 		("key\ with\ spaces\ : b", [("key with spaces ", "b")]),
+		("key\ with\ spaces\  : b", [("key with spaces ", "b")]),
+        ("\ before = n", [(" before", "b")])
 	)
+
+
+def test_space_surrounding_value():
+	_test_deserialize(
+		#("a = end ", [("a", "end ")]), # broken
+		("a = \ start", [("a", " start")]),
+	)
+
+
+def test_key_with_no__value():
+	_test_deserialize(
+		("a =", [("a", "")]),
+		#("a", [("a", "")]), # breaks in the parser line 154 SyntaxError
+	)
+
+
+def test_oddeties():
+	_test_deserialize(
+		("=", [("", "")]),    # Yes you can have a key with no value
+		("=b", [("", "b")]),  # and a value with no key
+	)
+
+
+def test_delimters_in_value():
+	_test_deserialize(
+        ("a = http://", [("a", "http://")]),
+        ("a = http\://", [("a", "http://")]),  # Some people believe you need to escape them Java says not
+        ("a = value=1", [("a", "value=1")]),
+        ("a = value\=1", [("a", "value=1")]),
+    )
+
+
+def test_special_characters_in_key():
+	_test_deserialize(
+        #("a = b\u0020", [("a", "b ")]), # Simple case within 127 range
+        #("a = b\u2026", [("a", "b…")]), # Bust index out of range # Complex outside of normal code pages
+    )
 
 
 def main():
